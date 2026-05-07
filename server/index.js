58c7e2cde548d637f37f5a58e3ff34211c6fbdca
Nandini Sihare <168717179+nandinisihare2332@users.noreply.github.com>
2026-05-07 22:31:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 require("dotenv").config({
     path:'.env'
 })
+const cors = require("cors");
 const app = require("./src/app");
 const {ConnectDB}=require("./src/config/db.config");
-
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 ConnectDB()
 const port = process.env.PORT || 4500
 app.listen(port,()=>{
     console.log(`the app is listen at http://localhost:${port}`);
 });
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
-}))
+
